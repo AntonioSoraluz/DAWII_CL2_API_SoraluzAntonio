e4cceb3b1ee9624cb5f2a79197732acbf2493ce2
Antonio Soraluz <soraluztorresjoseantonio@gmail.com>
2023-05-29 18:22:14 -0500

    Se creó el proyecto para el desarrollo de la CL2, manejo de apis

---
+++ src/main/java/com/soraluz/DawiiCl2ApiSoraluzAntonioApplication.java
@@ -0,0 +1,13 @@
+package com.soraluz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawiiCl2ApiSoraluzAntonioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawiiCl2ApiSoraluzAntonioApplication.class, args);
+	}
+
+}
